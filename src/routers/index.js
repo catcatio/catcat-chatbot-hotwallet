4fa4afb30f6235a0d812c5755e96d1e74721bed0
NiponChi <nipon.chi@gmail.com>
2018-08-24 22:04:31 +0700

    fixed path

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,8 +2,8 @@ module.exports = (config) => {
   const account = require('./account')(config)
   const router = require('express').Router()
 
-  router.use('account/:account_id', account)
-  router.use('accounts/:account_id', account)
+  router.use('/account/:account_id', account)
+  router.use('/accounts/:account_id', account)
 
   router.use('/', (req, res) => {
     res.status(200).send('OK')
